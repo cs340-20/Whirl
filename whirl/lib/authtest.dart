f17fb38b3540cfdf848004fac9fc7912090630b2
Tucker <tmiles7@vols.utk.edu>
2020-02-20 10:18:30 -0500

    work on auth

--- whirl/lib/authtest.dart
+++ whirl/lib/authtest.dart
@@ -1,2 +1,5 @@
-//testing stuff here...
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+final FirebaseAuth _auth = FirebaseAuth.instance;
