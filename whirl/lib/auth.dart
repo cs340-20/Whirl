b77a3e97f96ddbac0006c689fd4fa170fed82438
Tucker <tmiles7@vols.utk.edu>
2020-02-21 08:34:37 -0500

    auth work

--- whirl/lib/auth.dart
+++ whirl/lib/auth.dart
@@ -1,5 +1,20 @@
+final GoogleSignIn _googleSignIn = GoogleSignIn();
 import 'package:firebase_auth/firebase_auth.dart';
 
 String _email, _password;
 
 //save email and password from form...
+
+Future<FirebaseUser> _handleSignIn() async {
+  final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+  print("signed in " + user.displayName);
+  return user;
+}
