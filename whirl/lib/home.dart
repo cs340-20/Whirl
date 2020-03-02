4942939c2d760c2bb9c993fa0f446468d11cd4e0
Tucker <tuckermiles70@gmail.com>
2020-03-02 09:18:46 -0500

    firestore working correctly

--- whirl/lib/home.dart
+++ whirl/lib/home.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:whirl/auth.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 //list view of rides here...
 
 class ListRoute extends StatelessWidget {
   @override
-  // Widget build(BuildContext context) {
-  //   return new StreamBuilder(
-  //       stream: Firestore.instance.collection('Rides').document('K0vwi4gVKW2SBqgIT1xX').snapshots(),
-  //       builder: (context, snapshot) {
-  //         if (!snapshot.hasData) {
-  //           return new Text("Loading");
-  //         }
-  //         var userDocument = snapshot.data;
-  //         return new Text(userDocument["DestAsString"]);
-  //       }
-  //   );
-  // }
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('List Route'),
-      ),
-      body: Center(
-        child: Text("user " + curuser.email + " signed in"),
-      )
+    return new StreamBuilder(
+        stream: Firestore.instance.collection('Rides').document('K0vwi4gVKW2SBqgIT1xX').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return new Text("Loading");
+          }
+          var userDocument = snapshot.data;
+          return new Text(userDocument["DestAsString"]);
+        }
     );
   }
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text('List Route'),
+  //     ),
+  //     body: Center(
+  //       child: Text("user " + curuser.email + " signed in"),
+  //     )
+  //   );
+  // }
 }
