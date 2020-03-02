2692a878ee68a6a62f2023ec64a2f423b155c2ec
Tucker <tuckermiles70@gmail.com>
2020-03-02 09:55:07 -0500

    work on list build

--- whirl/lib/home.dart
+++ whirl/lib/home.dart
@@ -6,18 +6,41 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ListRoute extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return new StreamBuilder(
-        stream: Firestore.instance.collection('Rides').document('K0vwi4gVKW2SBqgIT1xX').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return new Text("Loading");
-          }
-          var userDocument = snapshot.data;
-          return new Text(userDocument["DestAsString"]);
+  // Widget build(BuildContext context) {
+  //   return new StreamBuilder(
+  //       stream: Firestore.instance.collection('Rides').document('K0vwi4gVKW2SBqgIT1xX').snapshots(),
+  //       builder: (context, snapshot) {
+  //         if (!snapshot.hasData) {
+  //           return new Text("Loading");
+  //         }
+  //         var userDocument = snapshot.data;
+  //         return new Text(userDocument["DestAsString"]);
+  //       }
+  //   );
+  // }
+    Widget build(BuildContext context) {
+    
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('K0vwi4gVKW2SBqgIT1xX').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError)
+          return new Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting: return new Text('Loading...');
+          default:
+            return new ListView(
+              children: snapshot.data.documents.map((DocumentSnapshot document) {
+                return new ListTile(
+                  title: new Text(document['DestAsString']),
+                  subtitle: new Text(document['DestAsString']),
+                );
+              }).toList(),
+            );
         }
+      },
     );
   }
+
   // Widget build(BuildContext context) {
   //   return Scaffold(
   //     appBar: AppBar(
