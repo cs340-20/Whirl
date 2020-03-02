09d42d95e0baf59ce100a35050668b47d5fbfdc5
Tucker <tmiles7@vols.utk.edu>
2020-03-01 20:40:19 -0500

    debugging firestore error

--- whirl/lib/home.dart
+++ whirl/lib/home.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:whirl/auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 //list view of rides here...
 
 class ListRoute extends StatelessWidget {
   @override
+  // Widget build(BuildContext context) {
+  //   return new StreamBuilder(
+  //       stream: Firestore.instance.collection('Rides').document('K0vwi4gVKW2SBqgIT1xX').snapshots(),
+  //       builder: (context, snapshot) {
+  //         if (!snapshot.hasData) {
+  //           return new Text("Loading");
+  //         }
+  //         var userDocument = snapshot.data;
+  //         return new Text(userDocument["DestAsString"]);
+  //       }
+  //   );
+  // }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
