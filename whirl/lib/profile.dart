2b8e877728e2b315a9984f325ab3661b288f18af
Tucker <tmiles7@vols.utk.edu>
2020-03-10 22:38:56 -0400

    profile page work

--- whirl/lib/profile.dart
+++ whirl/lib/profile.dart
@@ -20,16 +20,6 @@ class ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar (
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: () {},
-          )
-        ],
-      ),
       body: ListView(
         children: <Widget>[
           Column(
@@ -38,14 +28,14 @@ class ProfilePageState extends State<ProfilePage> {
               Hero(
                 tag: 'assets/WhirlLogo144.png',
                 child: Container (
-                  height: 125.0,
-                  width: 125.0,
+                  height: 150.0,
+                  width: 150.0,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(62.5),
+                    borderRadius: BorderRadius.circular(75),
                     image: DecorationImage(
                       fit: BoxFit.cover,
-                      image: AssetImage('assets/WhirlLogo144.png'),
-                    )
+                      image: AssetImage('assets/Avatars/TuckerBitmoji.jpg'),
+                    ),
                   ),
                 ),
               ),
@@ -60,12 +50,90 @@ class ProfilePageState extends State<ProfilePage> {
               ),
               SizedBox(height: 4.0),
               Text(
-                'Knoxville, TN'
+                'Knoxville, TN',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  color: Colors.grey,
+                ),
+              ),
+              Padding(
+                // padding: EdgeInsets.all(30),
+                padding: EdgeInsets.only(
+                  left: 100,
+                  top: 30,
+                  right: 100,
+                  bottom: 30,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    // Column(
+                    //   mainAxisAlignment: MainAxisAlignment.center,
+                    //   children: <Widget>[
+                    //     Text(
+                    //       '15K',
+                    //       style: TextStyle (
+                    //         fontFamily: 'Montserrat',
+                    //         fontWeight: FontWeight.bold,
+                    //       ),
+                    //     ),
+                    //     SizedBox(height: 5.0),
+                    //     Text(
+                    //       'FOLLS',
+                    //       style: TextStyle(
+                    //         fontFamily: 'Montserrat',
+                    //         color: Colors.grey,
+                    //       ),
+                    //     ),
+                    //   ],
+                    // ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '15',
+                          style: TextStyle (
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'TRIPS',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '10',
+                          style: TextStyle (
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'RATING',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),                                        
+                  ],
+                ),
               ),
             ],
-          )
+          ),
         ],
-      )
+      ),
     );
   }
 }
