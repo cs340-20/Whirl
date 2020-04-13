48bf6666e5d329c4c4f0da5f6cd64a4bef3fb943
Tucker <tmiles7@vols.utk.edu>
2020-04-12 20:34:53 -0400

    able to read ride data from firestore

--- whirl/lib/rides.dart
+++ whirl/lib/rides.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+class Ride {
+  String Dest;
+}
 
 class RidesPage extends StatefulWidget {
   @override
@@ -9,8 +14,68 @@ class RidesPageState extends State<RidesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Rides Page"),
+    return new Scaffold(
+      // appBar: new AppBar(
+      //   title: new Text("Current Rides"),
+      // ),
+      body: ListPage(),
+    );
+  }
+}
+
+class ListPage extends StatefulWidget {
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+
+  Future getRides() async {
+    //instantiate firestore instance -TM
+    var firestore = Firestore.instance;
+    
+    QuerySnapshot qn = await firestore.collection("Rides").getDocuments();
+
+    //essentially returns an array of the document snapshots -TM
+    return qn.documents;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FutureBuilder(
+        future: getRides(),
+        builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center (
+            child: Text("Loading"), //do circprogressindic instead -TM
+          );
+        }
+        else {
+          ListView.builder(
+            itemCount: snapshot.data.length, //length of document array returned from future -TM
+            itemBuilder: (_, index) {
+              return ListTile(
+                title: Text(snapshot.data[index].data["DestAsString"]),
+              );
+          });
+        }
+        return Text(snapshot.data[1].data["DestAsString"]);
+      }),
+    );
+  }
+}
+
+class DetailPage extends StatefulWidget {
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
