b2d955837ff9f8230de1fe795b2b9b013007d337
Tucker <tmiles7@vols.utk.edu>
2020-04-19 17:02:12 -0400

    removed unused package

--- whirl/lib/profile.dart
+++ whirl/lib/profile.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:whirl/rides.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
