e5236fd03af16c9ac3ca45a2a7c365d441b018d6
Tucker <tmiles7@vols.utk.edu>
2020-04-19 17:40:02 -0400

    added basic google map to map page

--- whirl/lib/map.dart
+++ whirl/lib/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -7,10 +8,33 @@ class MapPage extends StatefulWidget {
 
 class MapPageState extends State<MapPage> {
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Center(
+  //       child: Text("Map Page"),
+  //     );
+  // }
+
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text("Map Page"),
-      );
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
   }
 }
